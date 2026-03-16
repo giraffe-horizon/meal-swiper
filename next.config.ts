e217d7dfe96a3c1eb9c24d3ad039183c2fbcb7ee
Jarvis <jarvis@giraffe-horizon.dev>
2026-03-16 09:34:10 +0100

    fix: allow CF Analytics beacon in CSP script-src

--- next.config.ts
+++ next.config.ts
@@ -27,7 +27,7 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://static.cloudflareinsights.com",
               "style-src 'self' 'unsafe-inline' fonts.googleapis.com",
               "font-src 'self' fonts.gstatic.com",
               "img-src 'self' data: https:",
